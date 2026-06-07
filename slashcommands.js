d7544e01cb4edd5a0a6b2dd1e0ac52e7d042d167
xogametaim-prog <xogame.taim@gmail.com>
2026-06-07 17:55:21 +0300

    Update slashcommands.js

--- slashcommands.js
+++ slashcommands.js
@@ -1,28 +1,32 @@
 const { REST, Routes, SlashCommandBuilder } = require("discord.js");
 
 const commands = [
-    new SlashCommandBuilder()
-        .setName("pick_team")
-        .setDescription("اختيار منتخبك"),
+  new SlashCommandBuilder()
+    .setName("worldcup")
+    .setDescription("معلومات كأس العالم 2026"),
 
-    new SlashCommandBuilder()
-        .setName("my_team")
-        .setDescription("عرض منتخبك")
+  new SlashCommandBuilder()
+    .setName("pick_team")
+    .setDescription("اختر منتخبك (مرة واحدة فقط)"),
+
+  new SlashCommandBuilder()
+    .setName("my_team")
+    .setDescription("عرض منتخبك المختار")
 ].map(c => c.toJSON());
 
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
-    try {
-        console.log("Deploying commands...");
+  try {
+    console.log("Deploying slash commands...");
 
-        await rest.put(
-            Routes.applicationCommands(process.env.CLIENT_ID),
-            { body: commands }
-        );
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
 
-        console.log("Commands deployed!");
-    } catch (err) {
-        console.error(err);
-    }
+    console.log("Slash commands loaded ✔");
+  } catch (err) {
+    console.error(err);
+  }
 })();
